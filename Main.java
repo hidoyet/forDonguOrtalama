70b1feeeb487bfada2ec0ece2a47ff11067ac771
hidoyet <102352895+hidoyet@users.noreply.github.com>
2022-03-30 15:46:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package forDongusuTamBolunenSayi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+			//3 ve 4 e tam bölünen sayýlarýn ortalmasýný alan program
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("sayýyý giriniz");
+		int number=scanner.nextInt(),toplam=0,sum=0;
+		System.out.println("sayýyý giriniz");
+		for(int i=0;i<=number;i++) {
+			if(i%3==0&&i%4==0) {
+				int kontrol=1;
+				toplam=toplam+i;				
+				kontrol++;
+				sum=toplam/kontrol;
+			}
+			
+			
+		}
+		System.out.println(sum);
+	}
+
+}
